3df4273f0866268aa627bd53aa9bf3065c9a0486
endleees <fess0506@gmail.com>
2023-03-16 16:06:21 +0300

    Setup draw

---
+++ my-app/src/data/stylePolygons.js
@@ -0,0 +1,18 @@
+const styles = [
+  {
+    strokeColor: '#ff00ff', strokeOpacity: 0.7, strokeWidth: 3, fillColor: '#ff00ff', fillOpacity: 0.4,
+  },
+  {
+    strokeColor: '#ff0000', strokeOpacity: 0.6, strokeWidth: 6, fillColor: '#ff0000', fillOpacity: 0.3,
+  },
+  {
+    strokeColor: '#00ff00', strokeOpacity: 0.5, strokeWidth: 3, fillColor: '#00ff00', fillOpacity: 0.2,
+  },
+  {
+    strokeColor: '#0000ff', strokeOpacity: 0.8, strokeWidth: 5, fillColor: '#0000ff', fillOpacity: 0.5,
+  },
+  {
+    strokeColor: '#000000', strokeOpacity: 0.6, strokeWidth: 8, fillColor: '#000000', fillOpacity: 0.3,
+  },
+];
+export default styles
